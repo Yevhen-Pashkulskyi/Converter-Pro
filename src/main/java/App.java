35e82752ba99ccae8c998038a3d40209ad07c850
Pashkulskyi Yevhen <pashkulskyevhen@gmail.com>
2025-07-28 00:18:23 +0300

    Added convert method "milli to kilometre"

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -3,5 +3,15 @@ package main.java;
 import java.util.Scanner;
 
 public class App {
+    private static final double MILLI = 1.60934;
+    private static Scanner sc = new Scanner(System.in);
 
+    public static void main(String[] args) {
+        double milli = sc.nextDouble();
+        double res = milliToKilometre(milli);
+    }
+
+    private static double milliToKilometre(double milli) {
+        return milli * MILLI;
+    }
 }
