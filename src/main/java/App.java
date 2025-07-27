fd1cdafefd7cc7f265f20be15f9bc3094bf5597e
Pashkulskyi Yevhen <pashkulskyevhen@gmail.com>
2025-07-28 00:27:36 +0300

    Created two mwthods "kilometre to milli" and "print"

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -7,11 +7,25 @@ public class App {
     private static Scanner sc = new Scanner(System.in);
 
     public static void main(String[] args) {
+        System.out.print("Input milli: ");
         double milli = sc.nextDouble();
         double res = milliToKilometre(milli);
+        print(res,"Kilometre: ");
+
+        System.out.print("Input kilometre: ");
+        double kilometre = sc.nextDouble();
+        res = kilometreToMilli(kilometre);
+        print(res, "Milli: ");
     }
 
     private static double milliToKilometre(double milli) {
         return milli * MILLI;
+    }
+
+    private static double kilometreToMilli(double kilometre) {
+        return kilometre / MILLI;
+    }
+    private static void print(double res, String message) {
+        System.out.println(message + res);
     }
 }
